2b977d3aad602dd34fece361cd12678402733299
Panayot <panayot.todorov@gmail.com>
2017-07-19 11:43:50 +0300

    Added deploy script

---
+++ Linux.WebApi/LinuxDeploy.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set PrivateKeyLocation="C:\Users\Panayot\Documents\linux_amazon.ppk"
+set CertificateFileLocation="/mnt/c/Users/Panayot/Documents/linux_amazon.pem"
+
+for %%* in (.) do set CurrentDirName=%%~nx*
+set OutputFolder="%tmp%\%CurrentDirName%"
+set OutputZipFile="%tmp%\%CurrentDirName%.zip"
+set RemoteHost="ubuntu@ec2-54-229-219-60.eu-west-1.compute.amazonaws.com"
+set RemoteLocation="/home/ubuntu"
+
+dotnet restore
+dotnet publish -o "%OutputFolder%"
+
+powershell -command "& {&'Compress-Archive' -Path %OutputFolder% -DestinationPath %OutputZipFile%}"
+rmdir /s /q %OutputFolder%
+
+pscp -i %PrivateKeyLocation% -pw 4ubakatA %OutputZipFile% %RemoteHost%:%RemoteLocation%
+del /q %OutputZipFile%
+
+bash -c "ssh -i %CertificateFileLocation% %RemoteHost% 'sudo rm -rf %CurrentDirName% ; unzip %CurrentDirName%.zip ; rm -r %CurrentDirName%.zip ; sudo service supervisor restart'"
+
+pause
